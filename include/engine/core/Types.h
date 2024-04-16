07c6c7a0e5c413d56eb2f080af4452a273c2a935
Dmitry Sapelnikov <ds@multiconsult.no>
2024-04-16 11:16:04 +0200

    Added: comments

--- include/engine/core/Types.h
+++ include/engine/core/Types.h
@@ -4,12 +4,15 @@
 namespace gltut
 {
 
+// 32-bit integer
 using int32 = __int32;
 static_assert(sizeof(int32) == 4, "The size of int32 is not 4 bytes");
 
+// 32-bit unsigned integer
 using u32 = unsigned __int32;
 static_assert(sizeof(u32) == 4, "The size of u32 is not 4 bytes");
 
+// 32-bit float
 static_assert(sizeof(float) == 4, "The size of float is not 4 bytes");
 
 // End of the namespace gltut
