53af2e0f20d772d5bb330d1953c672db86f5f2ff
Dmitry Sapelnikov <ds@multiconsult.no>
2024-04-23 10:21:48 +0200

    Added: FLOAT_EPSILON constant

--- include/engine/math/Constants.h
+++ include/engine/math/Constants.h
@@ -1,6 +1,8 @@
 #ifndef OPENGL_TUTORIALS_MATH_CONSTANTS_H
 #define OPENGL_TUTORIALS_MATH_CONSTANTS_H
 
+#include <limits>
+
 namespace gltut
 {
 // Constants
@@ -9,6 +11,9 @@ constexpr float PI = float(3.14159265358979323846);
 
 /// The value of sqrt(2)
 constexpr float SQRT2 = float(1.41421356237309504880);
+
+/// Float epsilon
+constexpr float FLOAT_EPSILON = std::numeric_limits<float>::epsilon();
 
 // End of namespace gltut
 }
