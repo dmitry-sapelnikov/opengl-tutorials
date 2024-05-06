a0bc701d4aead8dd753d51e22c94aacd9e1ddc10
Dmitry Sapelnikov <ds@multiconsult.no>
2024-05-06 15:32:09 +0200

    Added: createSphereMesh

--- include/engine/MeshCreation.h
+++ include/engine/MeshCreation.h
@@ -21,6 +21,17 @@ namespace gltut
 */
 Mesh* createBoxMesh(Scene& scene, float width, float height, float depth) noexcept;
 
+/**
+	\brief Creates a sphere mesh with smooth normals
+
+	\param scene The scene where the mesh will be created
+	\param radius The radius of the sphere, must be greater than 0
+	\param subdivisions The number of subdivisions for the sphere, must be greater than 0
+	
+	\return The mesh if it was created successfully, nullptr otherwise
+*/
+Mesh* createSphereMesh(Scene& scene, float radius, u32 subdivisions) noexcept;
+
 // End of the namespace gltut
 }
 
--- src/engine/MeshCreation.cpp
+++ src/engine/MeshCreation.cpp
@@ -1,8 +1,63 @@
 // Includes
 #include "engine/MeshCreation.h"
+#include <array>
+#include <vector>
 
 namespace gltut
 {
+
+// Local functions
+namespace
+{
+
+void addFacesBetweenCircles(
+	u32 circle1Offset,
+	u32 circle2Offset,
+	u32 circleSegmentsCount,
+	std::vector<u32>& result)
+{
+	u32 prevIndex = circleSegmentsCount - 1;
+	for (u32 index = 0; index < circleSegmentsCount; ++index)
+	{
+		result.push_back(circle1Offset + prevIndex);
+		result.push_back(circle1Offset + index);
+		result.push_back(circle2Offset + index);
+
+		result.push_back(circle2Offset + index);
+		result.push_back(circle2Offset + prevIndex);
+		result.push_back(circle1Offset + prevIndex);
+
+		prevIndex = index;
+	}
+}
+
+void addFacesBetweenPointAndCircle(
+	u32 pointOffset,
+	u32 circleOffset,
+	u32 circleSegmentsCount,
+	bool flipNormals,
+	std::vector<u32>& result)
+{
+	u32 prevIndex = circleSegmentsCount - 1;
+	for (u32 index = 0; index < circleSegmentsCount; ++index)
+	{
+		if (flipNormals)
+		{
+			result.push_back(circleOffset + index);
+			result.push_back(circleOffset + prevIndex);
+			result.push_back(pointOffset);
+		}
+		else
+		{
+			result.push_back(circleOffset + prevIndex);
+			result.push_back(circleOffset + index);
+			result.push_back(pointOffset);
+		}
+		prevIndex = index;
+	}
+}
+// End of the anonymous namespace
+}
 
 // Global functions
 Mesh* createBoxMesh(Scene& scene, float x, float y, float z) noexcept
@@ -37,17 +92,17 @@ Mesh* createBoxMesh(Scene& scene, float x, float y, float z) noexcept
 		 x,  y,  z,  1.0f, 0.0f,  0.0f, 1.0f, 1.0f,
 		 x,  y, -z,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
 
-		// Top
-		-x,  y, -z,  0.0f, 1.0f,  0.0f, 0.0f, 0.0f,
-		 x,  y, -z,  0.0f, 1.0f,  0.0f, 1.0f, 0.0f,
-		 x,  y,  z,  0.0f, 1.0f,  0.0f, 1.0f, 1.0f,
-		-x,  y,  z,  0.0f, 1.0f,  0.0f, 0.0f, 1.0f,
+		 // Top
+		 -x,  y, -z,  0.0f, 1.0f,  0.0f, 0.0f, 0.0f,
+		  x,  y, -z,  0.0f, 1.0f,  0.0f, 1.0f, 0.0f,
+		  x,  y,  z,  0.0f, 1.0f,  0.0f, 1.0f, 1.0f,
+		 -x,  y,  z,  0.0f, 1.0f,  0.0f, 0.0f, 1.0f,
 
-		// Bottom
-		-x, -y, -z,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-		 x, -y, -z,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-		 x, -y,  z,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
-		-x, -y,  z,  0.0f, -1.0f, 0.0f, 0.0f, 1.0f
+		 // Bottom
+		 -x, -y, -z,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+		  x, -y, -z,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		  x, -y,  z,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+		 -x, -y,  z,  0.0f, -1.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	u32 indices[] =
@@ -61,6 +116,65 @@ Mesh* createBoxMesh(Scene& scene, float x, float y, float z) noexcept
 	};
 
 	return scene.createMesh(VERTEX_FORMAT_POS3_NORM3_TEX2, 24, vertices, 36, indices);
+}
+
+Mesh* createSphereMesh(Scene& scene, float radius, u32 subdivisions) noexcept
+{
+	GLTUT_CATCH_ALL_BEGIN
+		GLTUT_CHECK(radius > 0.0f, "Radius must be > 0");
+		GLTUT_CHECK(subdivisions >= 1, "Subdivisions must be >= 1");
+
+		using Vertex = std::array<float, 8>;
+
+		std::vector<Vertex> vertices;
+		vertices.push_back({ 0.0f, -radius, 0.0f, 0.0f, -1.0f, 0.0f, 0.5f, 0.0f });
+
+		const u32 latSubdivisions = 2 * subdivisions;
+		const u32 lonSubdivisions = 4 * subdivisions;
+
+		const float angleDelta = PI / latSubdivisions;
+		for (u32 latInd = 1; latInd < latSubdivisions; ++latInd)
+		{
+			float latitude = angleDelta * static_cast<float>(latInd) - 0.5f * PI;
+			for (u32 lonInd = 0; lonInd < lonSubdivisions; ++lonInd)
+			{
+				const float longitude = angleDelta * static_cast<float>(lonInd);
+				const Vector3 normal = setDistanceAzimuthInclination({ 1.0, longitude, latitude });
+
+				// setDistanceAzimuthInclination uses z-axis as the up vector, so
+				// so we need to swap the x and z components to use y-axis as the up vector
+				vertices.push_back({
+					radius * normal.x,
+					radius * normal.z,
+					radius * normal.y,
+					normal.x,
+					normal.z,
+					normal.y,
+					static_cast<float>(lonInd) / lonSubdivisions,
+					static_cast<float>(latInd) / latSubdivisions });
+			}
+		}
+		vertices.push_back({ 0.0f, radius, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f });
+
+		std::vector<u32> indices;
+		for (u32 latitudeIndex = 0; latitudeIndex < latSubdivisions - 2; ++latitudeIndex)
+		{
+			addFacesBetweenCircles(latitudeIndex * lonSubdivisions + 1, (latitudeIndex + 1) * lonSubdivisions + 1, lonSubdivisions, indices);
+		}
+
+		const u32 vertexCount = static_cast<u32>(vertices.size());
+		addFacesBetweenPointAndCircle(0, 1, lonSubdivisions, true, indices);
+		addFacesBetweenPointAndCircle(vertexCount - 1, vertexCount - 1 - lonSubdivisions, lonSubdivisions, false, indices);
+
+		return scene.createMesh(
+			VERTEX_FORMAT_POS3_NORM3_TEX2,
+			vertexCount,
+			vertices[0].data(),
+			static_cast<u32>(indices.size()),
+			indices.data());
+
+	GLTUT_CATCH_ALL_END("Failed to create a sphere mesh")
+	return nullptr;
 }
 
 // End of the namespace gltut
