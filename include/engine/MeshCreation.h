87d96802befe519a845aa732689f88bb46ed6af3
Dmitry Sapelnikov <ds@multiconsult.no>
2024-05-06 15:36:40 +0200

    Improvements in mesh functions

--- include/engine/MeshCreation.h
+++ include/engine/MeshCreation.h
@@ -18,6 +18,8 @@ namespace gltut
 
 	\note The mesh is created with the following vertex format:
 	VertexFormat::VERTEX_FORMAT_POS3_NORM3_TEX2
+
+	\note Negative dimentions produce an inverted mesh
 */
 Mesh* createBoxMesh(Scene& scene, float width, float height, float depth) noexcept;
 
@@ -25,10 +27,15 @@ Mesh* createBoxMesh(Scene& scene, float width, float height, float depth) noexce
 	\brief Creates a sphere mesh with smooth normals
 
 	\param scene The scene where the mesh will be created
-	\param radius The radius of the sphere, must be greater than 0
+	\param radius The radius of the sphere
 	\param subdivisions The number of subdivisions for the sphere, must be greater than 0
 	
 	\return The mesh if it was created successfully, nullptr otherwise
+
+	\note The mesh is created with the following vertex format:
+	VertexFormat::VERTEX_FORMAT_POS3_NORM3_TEX2
+
+	\note Negative radius produces an inverted mesh
 */
 Mesh* createSphereMesh(Scene& scene, float radius, u32 subdivisions) noexcept;
 
--- src/engine/MeshCreation.cpp
+++ src/engine/MeshCreation.cpp
@@ -62,66 +62,68 @@ void addFacesBetweenPointAndCircle(
 // Global functions
 Mesh* createBoxMesh(Scene& scene, float x, float y, float z) noexcept
 {
-	x *= 0.5f;
-	y *= 0.5f;
-	z *= 0.5f;
+	GLTUT_CATCH_ALL_BEGIN
+		x *= 0.5f;
+		y *= 0.5f;
+		z *= 0.5f;
 
-	const float vertices[] =
-	{
-		// Front
-		-x, -y,  z,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-		 x, -y,  z,  0.0f, 0.0f,  1.0f, 1.0f, 0.0f,
-		 x,  y,  z,  0.0f, 0.0f,  1.0f, 1.0f, 1.0f,
-		-x,  y,  z,  0.0f, 0.0f,  1.0f, 0.0f, 1.0f,
+		const float vertices[] =
+		{
+			// Front
+			-x, -y,  z,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+			 x, -y,  z,  0.0f, 0.0f,  1.0f, 1.0f, 0.0f,
+			 x,  y,  z,  0.0f, 0.0f,  1.0f, 1.0f, 1.0f,
+			-x,  y,  z,  0.0f, 0.0f,  1.0f, 0.0f, 1.0f,
 
-		// Back
-		-x, -y, -z,  0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-		 x, -y, -z,  0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
-		 x,  y, -z,  0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
-		-x,  y, -z,  0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+			// Back
+			-x, -y, -z,  0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+			 x, -y, -z,  0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+			 x,  y, -z,  0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+			-x,  y, -z,  0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
 
-		// Left
-		-x, -y, -z, -1.0f, 0.0f,  0.0f, 0.0f, 0.0f,
-		-x, -y,  z, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
-		-x,  y,  z, -1.0f, 0.0f,  0.0f, 1.0f, 1.0f,
-		-x,  y, -z, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+			// Left
+			-x, -y, -z, -1.0f, 0.0f,  0.0f, 0.0f, 0.0f,
+			-x, -y,  z, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+			-x,  y,  z, -1.0f, 0.0f,  0.0f, 1.0f, 1.0f,
+			-x,  y, -z, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
 
-		// Right
-		 x, -y, -z,  1.0f, 0.0f,  0.0f, 0.0f, 0.0f,
-		 x, -y,  z,  1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
-		 x,  y,  z,  1.0f, 0.0f,  0.0f, 1.0f, 1.0f,
-		 x,  y, -z,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+			// Right
+			 x, -y, -z,  1.0f, 0.0f,  0.0f, 0.0f, 0.0f,
+			 x, -y,  z,  1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+			 x,  y,  z,  1.0f, 0.0f,  0.0f, 1.0f, 1.0f,
+			 x,  y, -z,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
 
-		 // Top
-		 -x,  y, -z,  0.0f, 1.0f,  0.0f, 0.0f, 0.0f,
-		  x,  y, -z,  0.0f, 1.0f,  0.0f, 1.0f, 0.0f,
-		  x,  y,  z,  0.0f, 1.0f,  0.0f, 1.0f, 1.0f,
-		 -x,  y,  z,  0.0f, 1.0f,  0.0f, 0.0f, 1.0f,
+			 // Top
+			 -x,  y, -z,  0.0f, 1.0f,  0.0f, 0.0f, 0.0f,
+			  x,  y, -z,  0.0f, 1.0f,  0.0f, 1.0f, 0.0f,
+			  x,  y,  z,  0.0f, 1.0f,  0.0f, 1.0f, 1.0f,
+			 -x,  y,  z,  0.0f, 1.0f,  0.0f, 0.0f, 1.0f,
 
-		 // Bottom
-		 -x, -y, -z,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-		  x, -y, -z,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-		  x, -y,  z,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
-		 -x, -y,  z,  0.0f, -1.0f, 0.0f, 0.0f, 1.0f
-	};
+			 // Bottom
+			 -x, -y, -z,  0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			  x, -y, -z,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+			  x, -y,  z,  0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+			 -x, -y,  z,  0.0f, -1.0f, 0.0f, 0.0f, 1.0f
+		};
 
-	u32 indices[] =
-	{
-		0, 1, 2, 2, 3, 0,
-		4, 5, 6, 6, 7, 4,
-		8, 9, 10, 10, 11, 8,
-		12, 13, 14, 14, 15, 12,
-		16, 17, 18, 18, 19, 16,
-		20, 21, 22, 22, 23, 20
-	};
+		u32 indices[] =
+		{
+			0, 1, 2, 2, 3, 0,
+			4, 5, 6, 6, 7, 4,
+			8, 9, 10, 10, 11, 8,
+			12, 13, 14, 14, 15, 12,
+			16, 17, 18, 18, 19, 16,
+			20, 21, 22, 22, 23, 20
+		};
 
-	return scene.createMesh(VERTEX_FORMAT_POS3_NORM3_TEX2, 24, vertices, 36, indices);
+		return scene.createMesh(VERTEX_FORMAT_POS3_NORM3_TEX2, 24, vertices, 36, indices);
+	GLTUT_CATCH_ALL_END("Failed to create a box mesh")
+	return nullptr;
 }
 
 Mesh* createSphereMesh(Scene& scene, float radius, u32 subdivisions) noexcept
 {
 	GLTUT_CATCH_ALL_BEGIN
-		GLTUT_CHECK(radius > 0.0f, "Radius must be > 0");
 		GLTUT_CHECK(subdivisions >= 1, "Subdivisions must be >= 1");
 
 		using Vertex = std::array<float, 8>;
