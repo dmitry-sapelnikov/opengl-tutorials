b6e22d6196cb55676783f88d0b223f6161cdcf8b
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-08-08 00:39:10 +0200

    Refactored: Point2 classes

---
+++ include/engine/math/Vector2T.h
@@ -0,0 +1,86 @@
+#pragma once
+
+// Libraries
+#include "engine/core/Check.h"
+#include "engine/core/Types.h"
+
+namespace gltut
+{
+/// Represents a 2D Vector
+template <typename T>
+struct Vector2T
+{
+	/// Components of the vector
+	T x = 0;
+	T y = 0;
+
+	/// Default constructor
+	Vector2T() noexcept = default;
+
+	/// Constructor
+	constexpr Vector2T(T inX, T inY) noexcept :
+		x(inX),
+		y(inY)
+	{
+	}
+
+	/// + operator
+	Vector2T operator+(const Vector2T& v) const noexcept
+	{
+		return { x + v.x, y + v.y };
+	}
+
+	/// += operator
+	Vector2T& operator+=(const Vector2T& v) noexcept
+	{
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+
+	/// - operator
+	Vector2T operator-(const Vector2T& v) const noexcept
+	{
+		return { x - v.x, y - v.y };
+	}
+
+	/// -= operator
+	Vector2T& operator-=(const Vector2T& v) noexcept
+	{
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+
+	/// - operator
+	Vector2T operator-() const noexcept
+	{
+		return { -x, -y };
+	}
+
+	/// operator []
+	T& operator[](u32 i) noexcept
+	{
+		GLTUT_ASSERT(i < 2);
+		return (&x)[i];
+	}
+
+	/// operator []
+	const T& operator[](u32 i) const noexcept
+	{
+		GLTUT_ASSERT(i < 2);
+		return (&x)[i];
+	}
+
+	/// Checks if the point is zero
+	bool isZero() const noexcept
+	{
+		return x == 0 && y == 0;
+	}
+};
+
+/// Unsigned 32-bit point
+using Vector2 = Vector2T<float>;
+
+// End of the namespace gltut
+}
