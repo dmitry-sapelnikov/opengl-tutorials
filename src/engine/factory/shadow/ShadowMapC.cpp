ce3939d3d377e310a05a25a3c6eaa7233ca282e5
dmitry-sapelnikov <ds@multiconsult.no>
2025-08-19 09:51:01 +0200

    Fixed: directional light shadow map projection matrix

--- src/engine/factory/shadow/ShadowMapC.cpp
+++ src/engine/factory/shadow/ShadowMapC.cpp
@@ -136,18 +136,11 @@ void ShadowMapC::update() noexcept
 			1.0f;
 
 		mViewpoint.setProjectionMatrix(
-			Matrix4::orthographicProjectionMatrix(
-				50.0,
-				50.0,
-				mFrustumNear,
-				mFrustumFar));
-
-		/*mViewpoint.setProjectionMatrix(
 			Matrix4::perspectiveProjectionMatrix(
 				mLight.getOuterAngle() * 2.0f,
 				aspectRatio,
 				mFrustumNear,
-				mFrustumFar));*/
+				mFrustumFar));
 	}
 }
 
