8925540b5ca9438487e49e2c445b3e194caf98fe
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2025-09-01 21:53:58 +0200

    Fixed: 15_framebuffers test

--- src/tutorials/15_framebuffers/15_framebuffers.cpp
+++ src/tutorials/15_framebuffers/15_framebuffers.cpp
@@ -15,7 +15,6 @@ static const float POSITION_RANGE = 5.0f;
 ///	The program entry point
 int main()
 {
-	gltut::EngineImgui* imgui = nullptr;
 	try
 	{
 		std::unique_ptr<gltut::Engine> engine(gltut::createEngine(1024, 768));
@@ -30,49 +29,16 @@ int main()
 
 		GLTUT_CHECK(geometry != nullptr, "Failed to create geometry")
 
-		gltut::Shader* shader = renderer->getDevice()->getShaders()->load(
-			"assets/shader.vs",
-			"assets/shader.fs");
-
-		GLTUT_CHECK(shader != 0, "Failed to create shader program")
-		shader->setInt("texture1", 0);
-		shader->setInt("texture2", 1);
-
-		auto* materialBinding = renderer->createShaderBinding(shader);
-		GLTUT_CHECK(materialBinding, "Failed to create light shader material binding");
-		materialBinding->bind(
-			gltut::ShaderRendererBinding::Parameter::GEOMETRY_MATRIX,
-			"model");
-
-		materialBinding->bind(
-			gltut::ShaderRendererBinding::Parameter::VIEWPOINT_VIEW_MATRIX,
-			"view");
-
-		materialBinding->bind(
-			gltut::ShaderRendererBinding::Parameter::VIEWPOINT_PROJECTION_MATRIX,
-			"projection");
-
-		gltut::Texture* texture1 = renderer->getDevice()->getTextures()->load("assets/container.jpg", {});
-		GLTUT_CHECK(texture1 != nullptr, "Failed to load texture");
-
-		gltut::Texture* texture2 = renderer->getDevice()->getTextures()->load("assets/awesomeface.png");
-		GLTUT_CHECK(texture2 != nullptr, "Failed to load texture");
-
-		auto* material = renderer->createMaterial();
-		GLTUT_CHECK(material != nullptr, "Failed to create material");
-
-		auto* materialPass = material->createPass(0, materialBinding, 2);
-		GLTUT_CHECK(materialPass != nullptr, "Failed to create material pass");
-
-		materialPass->getTextures()->setTexture(texture1, 0);
-		materialPass->getTextures()->setTexture(texture2, 1);
+		auto* material = engine->getFactory()->getMaterial()->createFlatColorMaterial(true);
+		material->setColor(
+			renderer->getDevice()->getTextures()->load("assets/container.jpg", {}));
 
 		gltut::Rng rng;
 		std::vector<gltut::GeometryNode*> boxes;
 		std::vector<gltut::Vector3> boxPositions;
 		for (size_t i = 0; i < BOX_COUNT; ++i)
 		{
-			boxes.push_back(scene->createGeometry(geometry, material));
+			boxes.push_back(scene->createGeometry(geometry, material->getMaterial()));
 			boxPositions.emplace_back(
 				rng.nextFloat(-POSITION_RANGE, POSITION_RANGE),
 				rng.nextFloat(-POSITION_RANGE, POSITION_RANGE),
@@ -92,53 +58,51 @@ int main()
 		GLTUT_CHECK(controller.get() != nullptr, "Failed to create camera controller");
 		engine->getScene()->addCameraController(controller.get());
 
-		// Create deferred rendering
+
 		engine->getRenderer()->removeAllPasses();
-		gltut::Color clearColor{ 0.5f, 0.5f, 0.5f };
-		renderer->createPass(
+		const gltut::u32 textureSize = 512;
+		auto* colorTexture = renderer->getDevice()->getTextures()->create(
 			nullptr,
-			nullptr,
-			renderer->getDevice()->getFramebuffers()->getDefault(),
-			0,
-			&clearColor,
-			true,
-			nullptr);
+			{ textureSize, textureSize },
+			gltut::TextureFormat::RGBA,
+			{ gltut::TextureFilterMode::NEAREST,
+			gltut::TextureFilterMode::NEAREST,
+			gltut::TextureWrapMode::CLAMP_TO_EDGE });
 
-		gltut::Texture* rgbaTexture = renderer->getDevice()->getTextures()->create(
-			nullptr,
-			{ 512, 512 },
-			gltut::TextureFormat::RGB);
+		GLTUT_CHECK(colorTexture != nullptr, "Failed to create color texture");
 
-		gltut::Texture* depthTexture = renderer->getDevice()->getTextures()->create(
+		auto* depthTexture = renderer->getDevice()->getTextures()->create(
 			nullptr,
-			{ 512, 512 },
-			gltut::TextureFormat::FLOAT);
-
-		GLTUT_CHECK(rgbaTexture != nullptr, "Failed to create RGBA texture");
+			{ textureSize, textureSize },
+			gltut::TextureFormat::FLOAT,
+			{ gltut::TextureFilterMode::NEAREST,
+			gltut::TextureFilterMode::NEAREST,
+			gltut::TextureWrapMode::CLAMP_TO_EDGE });
 
 		auto* framebuffer = renderer->getDevice()->getFramebuffers()->create(
-			rgbaTexture,
+			colorTexture,
 			depthTexture);
+		GLTUT_CHECK(framebuffer, "Failed to create framebuffer");
 
-		gltut::Color redColor{ 1.0f, 0.0f, 0.0f, 1.0f };
-		auto pass = renderer->createPass(
+		gltut::Color clearColor(0.5f, 0.5f, 0.5f);
+		auto* renderToTexturePass = renderer->createPass(
 			scene->getActiveCameraViewpoint(),
 			scene->getRenderGroup(),
 			framebuffer,
 			0,
-			&redColor,
+			&clearColor,
+			true, // Depth clearing
+			nullptr,
 			true,
+			false);
+
+		GLTUT_CHECK(renderToTexturePass != nullptr, "Failed to create shadow map render pass");
+		renderer->setPassPriority(renderToTexturePass, 0);
+
+		auto* textureToWindowPass = engine->getFactory()->getRenderPass()->createTextureToWindowRenderPass(
+			colorTexture,
 			nullptr);
-
-		renderer->setPassPriority(pass, -1);
-
-		//engine->getFactory()->getRenderPass()->createTextureToWindowRenderPass(
-		//	rgbaTexture,
-		//	1,
-		//	nullptr);
-
-		imgui = gltut::createEngineImgui(engine.get());
-		GLTUT_CHECK(imgui != nullptr, "Failed to create ImGui instance");
+		renderer->setPassPriority(textureToWindowPass, 1000);
 
 		auto start = std::chrono::high_resolution_clock::now();
 		do
@@ -152,31 +116,9 @@ int main()
 				boxes[i]->setTransform(
 					gltut::Matrix4::translationMatrix(boxPositions[i]) * rotation);
 			}
-
-			imgui->newFrame();
-			ImGui::SetNextWindowPos({ 10, 10 }, ImGuiCond_FirstUseEver);
-			ImGui::SetNextWindowSize({ 200, 400 }, ImGuiCond_FirstUseEver);
-
-			ImGui::Begin("Settings");
-
-			ImGui::Text("FPS: %u", engine->getWindow()->getFPS());
-			ImGui::Text("Time: %.2f seconds", time);
-
-			// Plot the render texture
-			if (rgbaTexture != nullptr)
-			{
-				ImGui::Text("Render texture");
-				ImGui::Image(
-					depthTexture->getId(),
-					{ 200, 200 },
-					{ 0, 1 }, { 1, 0 });
-			}
-
-			ImGui::End();
 		} while (engine->update());
 	}
 	GLTUT_APPLICATION_CATCH;
 
-	gltut::deleteEngineImgui(imgui);
 	return 0;
 }
